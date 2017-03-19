849b1c3d1914556e6baa5e37c9003e2a136e0091
Mauricio Rojas <pinrojas@gmail.com>
2017-03-19 16:54:08 -0500

    Create setup.sh

---
+++ install/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -f /var/log/ansible/ansible-packet-nuagevns.log ]; then
+    echo "There is not data. Proceed setup"
+    git clone https://github.com/p1nrojas/packet-nuagevns ~/packet-nuagevns
+    touch /var/log/ansible/ansible-packet-nuagevns.log
+    ssh-keygen -t rsa -b 4096 -C "dev@nuage.io" -f ~/.ssh/id_rsa -q -N ""
+    /bin/bash
+else
+    echo "Caution: user data detected. skipping setup. Next time override CMD"
+fi
+
+    
