01621f44a2260d604cfc6d9456e86cfad86785f5
Mauricio Rojas <pinrojas@gmail.com>
2017-03-24 23:53:18 -0500

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -9,7 +9,7 @@ systemctl enable docker >> /tmp/install.log
 
 #Install weave
 echo "$(date) Installing weave..."
-curl -L git.io/weave -o /usr/local/bin/weave >> /tmp/install.log
+curl -fsSL git.io/weave -o /usr/local/bin/weave >> /tmp/install.log
 chmod a+x /usr/local/bin/weave >> /tmp/install.log
 weave launch --ipalloc-range 192.168.0.0/24 >> /tmp/install.log
 ip a add 192.168.0.1/24 dev weave >> /tmp/install.log
@@ -18,21 +18,26 @@ ip a add 192.168.0.1/24 dev weave >> /tmp/install.log
 echo "$(date) Creating containers..."
 mkdir -p ~/github/packet-nuagevns >> /tmp/install.log
 cd ~/github/packet-nuagevns >> /tmp/install.log
-curl -o Dockerfile https://raw.githubusercontent.com/p1nrojas/packet-nuagevns/master/install/Dockerfile >> /tmp/install.log
-curl -o vimrc https://raw.githubusercontent.com/p1nrojas/packet-nuagevns/master/install/vimrc >> /tmp/install.log
-curl -o bash_profile https://raw.githubusercontent.com/p1nrojas/packet-nuagevns/master/install/bash_profile >> /tmp/install.log
-curl -o setup.sh https://raw.githubusercontent.com/p1nrojas/packet-nuagevns/master/install/setup.sh >> /tmp/install.log
+curl -fsS -o Dockerfile https://raw.githubusercontent.com/p1nrojas/packet-nuagevns/master/install/Dockerfile >> /tmp/install.log
+curl -fsS -o vimrc https://raw.githubusercontent.com/p1nrojas/packet-nuagevns/master/install/vimrc >> /tmp/install.log
+curl -fsS -o bash_profile https://raw.githubusercontent.com/p1nrojas/packet-nuagevns/master/install/bash_profile >> /tmp/install.log
+curl -fsS -o setup.sh https://raw.githubusercontent.com/p1nrojas/packet-nuagevns/master/install/setup.sh >> /tmp/install.log
 
-#Asking packet.net token 
+#Asking packet.net token
+echo ">>"
+echo ">>"
 echo -n "Enter packet.net token (i.e MaKWKw8AxUASBTE7JKj1y7eM7qW4o9Sd ) and press [ENTER]: "
 read packet_token
-if [[ $packet_token =~ ^[A-Za-z0-9]{33}$ ]]; then echo $packet_token > .packet_token; else echo "Format isn't right, bye!"; exit 1; fi
+if [[ $packet_token =~ ^[A-Za-z0-9]{32}$ ]]; then echo $packet_token > .packet_token; else echo "Format isn't right, bye!"; exit 1; fi
 
 #Asking packet.net project id
+echo ">>"
+echo ">>"
 echo -n "Enter packet.net project ID (i.e. bed437ce-6ae7-6b3a-a8e0-163e13a12a32 ) and press [ENTER]: "
 read project_id
 if [[ $project_id =~ ^[a-z0-9]*-[a-z0-9]*-[a-z0-9]*-[a-z0-9]*-[a-z0-9]*$ ]]; then echo $project_id > .packet_project_id; else echo "Format isn't right, bye!"; exit 1; fi
 
+echo "$(date) Passed. Creating container image..."
 
 docker build -t p1nrojas/packet-nuagevns . >> /tmp/install.log
 
